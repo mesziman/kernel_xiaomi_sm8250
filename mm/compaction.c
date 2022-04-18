ed0e9dcfc5aa008211b7746c7ac402a176eb33aa
UtsavBalar1231 <utsavbalar1231@gmail.com>
2022-04-18 05:31:49 +0000

    mm: compaction: Use MI DRM notifier API
    Signed-off-by: UtsavBalar1231 <utsavbalar1231@gmail.com>

--- mm/compaction.c
+++ mm/compaction.c
@@ -23,7 +23,7 @@
 #include <linux/freezer.h>
 #include <linux/page_owner.h>
 #include <linux/psi.h>
-#include <linux/msm_drm_notify.h>
+#include <drm/drm_notifier_mi.h>
 #include <linux/moduleparam.h>
 #include <linux/time.h>
 #include <linux/workqueue.h>
@@ -2452,13 +2452,13 @@ module_param_named(compaction_screen_off_delay_ms,
 static unsigned long compaction_forced_timeout;
 
 
-static int msm_drm_notifier_callback(struct notifier_block *self,
+static int mi_drm_notifier_callback(struct notifier_block *self,
 				       unsigned long event, void *data)
 {
-	struct msm_drm_notifier *evdata = data;
+	struct mi_drm_notifier *evdata = data;
 	int *blank;
 
-	if (event != MSM_DRM_EVENT_BLANK && event != MSM_DRM_EARLY_EVENT_BLANK)
+	if (event != MI_DRM_EVENT_BLANK && event != MI_DRM_EARLY_EVENT_BLANK)
 		goto out;
 
 	if (!evdata || !evdata->data)
@@ -2466,9 +2466,7 @@ static int msm_drm_notifier_callback(struct notifier_block *self,
 
 	blank = evdata->data;
 	switch (*blank) {
-	case MSM_DRM_BLANK_POWERDOWN_CUST:
-	case MSM_DRM_BLANK_POWERDOWN:
-	case MSM_DRM_BLANK_NORMAL:
+	case MI_DRM_BLANK_POWERDOWN:
 		if (!screen_on)
 			goto out;
 		screen_on = false;
@@ -2478,7 +2476,7 @@ static int msm_drm_notifier_callback(struct notifier_block *self,
 					   msecs_to_jiffies(compaction_soff_delay_ms));
 		}
 		break;
-	case MSM_DRM_BLANK_UNBLANK_CUST:
+	case MI_DRM_BLANK_UNBLANK:
 		if (screen_on)
 			goto out;
 		screen_on = true;
@@ -2490,7 +2488,7 @@ out:
 }
 
 static struct notifier_block compaction_notifier_block = {
-	.notifier_call = msm_drm_notifier_callback,
+	.notifier_call = mi_drm_notifier_callback,
 };
 
 /* Compact all zones within a node */
@@ -2850,8 +2848,6 @@ static int __init kcompactd_init(void)
 }
 subsys_initcall(kcompactd_init)
 
-extern struct drm_panel *lcd_active_panel;
-
 static int  __init scheduled_compaction_init(void)
 {
 	compaction_wq = create_freezable_workqueue("compaction_wq");
@@ -2861,10 +2857,7 @@ static int  __init scheduled_compaction_init(void)
 
 	INIT_DELAYED_WORK(&compaction_work, do_compaction);
 
-	if (lcd_active_panel) {
-		drm_panel_notifier_register(lcd_active_panel,
-					    &compaction_notifier_block);
-	}
+	mi_drm_register_client(&compaction_notifier_block);
 
 	return 0;
 }
