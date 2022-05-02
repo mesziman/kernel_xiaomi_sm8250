0382c4a168d780e097f4cc1f0dccb5e9c0847e6b
Gabor Meszaros <meszaros.gabor.10k@gmail.com>
2022-05-02 15:23:38 +0200

    sched: inline core_ctl int walt code for upstream changes

--- include/linux/sched/core_ctl.h
+++ include/linux/sched/core_ctl.h
@@ -16,11 +16,11 @@ struct core_ctl_notif_data {
 	unsigned int cur_cap_pct[MAX_CLUSTERS];
 };
 
-#ifdef CONFIG_SCHED_CORE_CTL
-void core_ctl_check(u64 wallclock);
-int core_ctl_set_boost(bool boost);
-void core_ctl_notifier_register(struct notifier_block *n);
-void core_ctl_notifier_unregister(struct notifier_block *n);
+#ifdef CONFIG_SCHED_WALT
+extern void core_ctl_check(u64 wallclock);
+extern int core_ctl_set_boost(bool boost);
+extern void core_ctl_notifier_register(struct notifier_block *n);
+extern void core_ctl_notifier_unregister(struct notifier_block *n);
 #else
 static inline void core_ctl_check(u64 wallclock) {}
 static inline int core_ctl_set_boost(bool boost)
