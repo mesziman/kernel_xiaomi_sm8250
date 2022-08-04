e6e99550a3b550f0db63bae099f706ff7eb9676f
Gabor Meszaros <meszaros.gabor.10k@gmail.com>
2022-08-04 15:17:24 +0200

    fixup add zone stuff

--- block/blk-sysfs.c
+++ block/blk-sysfs.c
@@ -854,8 +854,6 @@ static void __blk_release_queue(struct work_struct *work)
 
 	blk_queue_free_zone_bitmaps(q);
 
-	blk_queue_free_zone_bitmaps(q);
-
 	if (!q->mq_ops) {
 		if (q->exit_rq_fn)
 			q->exit_rq_fn(q, q->fq->flush_rq);
