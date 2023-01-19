37395938454b9592bee9f78a08287ffc8676a53c
Gabor Meszaros <meszaros.gabor.10k@gmail.com>
2023-01-19 10:10:10 +0100

    walt: Update default window size to 5.4 kernel

--- kernel/sched/walt.h
+++ kernel/sched/walt.h
@@ -18,10 +18,12 @@
  * Tick interval becomes to 3333333 due to
  * rounding error when HZ=300.
  */
-#define DEFAULT_SCHED_RAVG_WINDOW (3333333 * 6)
+#define DEFAULT_SCHED_RAVG_WINDOW (3333333 * 5)
+#elif defined(CONFIG_HZ_100)
+#define DEFAULT_SCHED_RAVG_WINDOW 20000000
 #else
-/* Default window size (in ns) = 8ms */
-#define DEFAULT_SCHED_RAVG_WINDOW 8000000
+/* Min window size (in ns) = 16ms */
+#define DEFAULT_SCHED_RAVG_WINDOW 16000000
 #endif
 
 /* Max window size (in ns) = 1s */
