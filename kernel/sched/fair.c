76d3bd87320ff8243d195853243af705a5a8b746
Gabor Meszaros <meszaros.gabor.10k@gmail.com>
2023-01-19 21:47:11 +0100

    fixup! sched:improve: differentiate between binder and proc for low latency

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -4208,7 +4208,7 @@ place_entity(struct cfs_rq *cfs_rq, struct sched_entity *se, int initial)
 		vruntime -= thresh;
 #ifdef CONFIG_SCHED_WALT
 		if (entity_is_task(se)) {
-			if (per_task_boost(task_of(se)) == TASK_BOOST_STRICT_MAX)
+			if (per_task_boost(task_of(se)) == TASK_BOOST_STRICT_MAX) {
 				vruntime -= thresh;
 				vruntime -= sysctl_sched_latency;
 				se->vruntime = vruntime;
